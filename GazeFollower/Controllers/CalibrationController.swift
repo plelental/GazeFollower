75787316fae0e61e959f26ce0b3cd5776fe99320
Pawel Lelental <p.lelental@gmail.com>
2021-03-17 21:27:57 +0100

    Init of taking photo while testing calibration

--- GazeFollower/Controllers/CalibrationController.swift
+++ GazeFollower/Controllers/CalibrationController.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import ARKit
+import AVFoundation
 
 class CalibrationController: BaseController, ARSCNViewDelegate, ARSessionDelegate {
     
@@ -18,9 +19,36 @@ class CalibrationController: BaseController, ARSCNViewDelegate, ARSessionDelegat
     private var faceModel: FaceModel!
     private var calibrationPointModel = CalibrationPointModel()
     private var calibrationDataPointService = CalibrationDataPointService()
+    var photoSettings: AVCapturePhotoSettings!
     
+    
+    
+    // Depth variables
+    private let depthDataOutput = AVCaptureDepthDataOutput()
+    private let session = AVCaptureSession()
+    //    private let sessionQueue = DispatchQueue(label: "session queue", attributes: [], autoreleaseFrequency: .workItem)
+    
+    let photoOutput = AVCapturePhotoOutput()
+    let captureProcessor = PhotoCaptureProcessor()
+    
+    //
     @objc func touchedScreen(touch: UITapGestureRecognizer) {
         isScreenTouched = true;
+        calibrationView.session.pause()
+        configureSession()
+        photoSettings = AVCapturePhotoSettings.init(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
+        photoSettings.isAutoStillImageStabilizationEnabled = true
+        let group = DispatchGroup()
+        
+        group.enter()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { // Change `2.0` to the desired number of seconds.
+            self.photoOutput.capturePhoto(with: self.photoSettings, delegate: self.captureProcessor)
+            group.leave()
+        }
+        group.notify(queue: .main) {
+            self.setUpARSession(view: self.calibrationView)
+        }
+        
     }
     
     override func viewDidLoad() {
@@ -28,6 +56,8 @@ class CalibrationController: BaseController, ARSCNViewDelegate, ARSessionDelegat
         setUpARSCNView(view: calibrationView, viewDelegate: self, arSessionDelegate: self)
         faceModel = FaceModel(view: calibrationView)
         calibrationView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(self.touchedScreen(touch:))))
+        
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -41,6 +71,44 @@ class CalibrationController: BaseController, ARSCNViewDelegate, ARSessionDelegat
         setUpARSession(view: calibrationView)
     }
     
+    func configureSession(){
+        // Select a depth-capable capture device.
+        session.beginConfiguration()
+        let deviceSession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInDualCamera, .builtInTelephotoCamera, .builtInWideAngleCamera], mediaType: AVMediaType.video, position: .unspecified)
+        for device in (deviceSession.devices){
+            if device.position == AVCaptureDevice.Position.front {
+                
+                do{
+                    let input = try AVCaptureDeviceInput(device: device)
+                    if session.canAddInput(input) {
+                        session.addInput(input)
+                        
+                        if session.canAddOutput(photoOutput) {
+                            session.addOutput(photoOutput)
+                        }
+                        session.commitConfiguration()
+                        session.startRunning()
+                    }
+                    
+                }catch let avError {
+                    print (avError)
+                }
+            }
+        }
+        //        }
+        //        self.session.beginConfiguration()
+        //
+        //
+        //        guard self.session.canAddOutput(photoOutput) else { return }
+        //        self.session.sessionPreset = .photo
+        //        //        self.session.addOutput(photoOutput)
+        //        session.addOutput(photoOutput)
+        //
+        //
+        //        self.session.commitConfiguration()
+        //        self.session.startRunning()
+        
+    }
     override func viewWillDisappear(_ animated: Bool) {
         super.hideNavigationBar()
         calibrationView.session.pause()
@@ -53,7 +121,9 @@ class CalibrationController: BaseController, ARSCNViewDelegate, ARSessionDelegat
         }
         
         faceModel.update(faceAnchor: faceAnchor, view: calibrationView, node: node)
-                
+        
+        
+        
         DispatchQueue.main.async(execute: { () -> Void in
             self.gazePoint.center = self.faceModel.estimationPointOnTheScreen
             self.calibrationPoint.center = self.calibrationPointModel.point
@@ -65,7 +135,14 @@ class CalibrationController: BaseController, ARSCNViewDelegate, ARSessionDelegat
                     estimationPoint: self.gazePoint.center,
                     distance: 0,
                     calibrationStep: self.calibrationPointModel.calibrationStep ?? CalibrationStep.OutOfTheScreen)
-               
+                
+                
+                //                photoSettings.flashMode = .auto
+                //                photoSettings.isAutoStillImageStabilizationEnabled =
+                //                    self.photoOutput.isStillImageStabilizationSupported
+                // Shoot the photo, using a custom class to handle capture delegate callbacks.
+                ///
+                
                 self.calibrationPointModel.update()
             }
         })
@@ -73,15 +150,15 @@ class CalibrationController: BaseController, ARSCNViewDelegate, ARSessionDelegat
     
     private func renderScreenPoints() {
         let screenGazePoint = ScreenPointModel(cornerRadius: 20,
-                                           shadowOpacity: 1,
-                                           shadowOffset: .zero,
-                                           shadowRadius: 20,
-                                           shadowPath: UIBezierPath(rect: gazePoint.bounds).cgPath,
-                                           width: 40,
-                                           height: 40,
-                                           x: 0,
-                                           y: 0,
-                                           backgroundColor: ColorHelper.UIColorFromRGB(0x1273DE))
+                                               shadowOpacity: 1,
+                                               shadowOffset: .zero,
+                                               shadowRadius: 20,
+                                               shadowPath: UIBezierPath(rect: gazePoint.bounds).cgPath,
+                                               width: 40,
+                                               height: 40,
+                                               x: 0,
+                                               y: 0,
+                                               backgroundColor: ColorHelper.UIColorFromRGB(0x1273DE))
         
         let screenCalibrationPoint = ScreenPointModel(cornerRadius: 25,
                                                       width: 50,
@@ -96,16 +173,17 @@ class CalibrationController: BaseController, ARSCNViewDelegate, ARSessionDelegat
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
-        
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
+}
+class PhotoCaptureProcessor: NSObject, AVCapturePhotoCaptureDelegate {
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        var z = photo.previewPixelBuffer
+        var t = photo.previewCGImageRepresentation()
+        var q = photo.fileDataRepresentation()
+        var o = UIImage(data: q!)
+        var b = 0
         
     }
 }
